6cff6fa34b6d2b02736186e77c69db3be0f8a390
UAnirudh <uanirudh0811@gmail.com>
2026-03-04 17:14:36 -0800

    Scheduler has HTML now, looks really good
    Claude devolved HTML while I did the back end, looks really nice.

--- Testing/TestApp.py
+++ Testing/TestApp.py
@@ -1,8 +1,19 @@
 import flask
+from flask import render_template
+import TestData
 app = flask.Flask(__name__)
 
-@app.route('/home')
-def hello():
-    return 'Hello, World!'
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!'
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/schedule')
+def data():
+    return flask.jsonify(TestData.generate_schedule(TestData.assignments))
+
 if __name__ == '__main__':
     app.run(debug=True)
