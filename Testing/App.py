8c499c5b424ac53a4c48722df08c94b5feec5fd1
UAnirudh <uanirudh0811@gmail.com>
2026-03-12 17:53:04 -0700

    Updated App.py

--- Testing/App.py
+++ Testing/App.py
@@ -11,45 +11,10 @@ CANVAS_TOKEN = os.getenv("CANVAS_TOKEN")
 CANVAS_BASE = "https://canvas.instructure.com/api/v1"
 app = flask.Flask(__name__)
 
-# @app.route('/')
-# def hello():
-#     return 'Hello, World!'
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# @app.route('/schedule')
-# def data():
-#     return flask.jsonify(TestData.generate_schedule(TestData.assignments))
-
-# @app.route('/courses')
-# def get_courses():
-#     headers = {"Authorization": f"Bearer {CANVAS_TOKEN}"}
-#     response = requests.get(f"{CANVAS_BASE}/courses", headers=headers)
-#     return flask.jsonify(response.json())
-
-# @app.route('/assignments')
-# def get_assignments():
-#     headers = {"Authorization": f"Bearer {CANVAS_TOKEN}"}
-#     response = requests.get(f"{CANVAS_BASE}/courses/14365743/assignments", headers=headers)
-#     return flask.jsonify(response.json())
-
-# @app.route('/clean')
-# def get_clean_assignments():
-#     headers = {"Authorization": f"Bearer {CANVAS_TOKEN}"}
-#     response = requests.get(f"{CANVAS_BASE}/courses/14365743/assignments", headers=headers)
-#     assignments = response.json()  
-#     clean = []
-#     for a in assignments:
-#         clean.append({
-#             "name": a["name"],
-#             "due_at": a["due_at"],
-#             "points_possible": a["points_possible"],
-#             "course_id": a["course_id"]
-#         })
-#     sorted_clean = sorted(clean, key=lambda x: x['due_at'])
-#     return flask.jsonify(sorted_clean)
 @app.route('/live')
 def get_live_schedule():
     headers = {"Authorization": f"Bearer {CANVAS_TOKEN}"}
@@ -58,7 +23,11 @@ def get_live_schedule():
     
     schedule = []
     today = datetime.now(timezone.utc)
-    
+    course_response = requests.get(f"{CANVAS_BASE}/courses", headers=headers)
+    courses = course_response.json()
+    course_map = {}
+    for c in courses:
+        course_map[c["id"]] = c["name"]
     for a in assignments:
         due_str = a["due_at"]  # "2026-03-31T05:59:59Z"
         due_date = datetime.fromisoformat(due_str.replace("Z", "+00:00"))
@@ -74,7 +43,7 @@ def get_live_schedule():
         rounded_minutes = round(raw_minutes / 30) * 30
         schedule.append({
             "title": a["name"],
-            "course": a["course_id"],
+            "course": course_map.get(a["course_id"], "Unknown Course"),
             "due_date": due_str[:10],
             "points_possible": a["points_possible"],
             "priority": priority,
